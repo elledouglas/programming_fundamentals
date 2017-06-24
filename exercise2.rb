e7bb20dc8e8092d83b70a7af954657b0de986bbc
Kathleen <kathleenmcclay@gmail.com>
2017-06-24 18:51:35 -0400

    exercise3 complete

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+#1. How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+
+
+puts "The tip amount for 55 dollars is #{55 * 0.2}"
+
+# Try adding a string and an interger with the + operator. Find a way to convert into a string
+
+puts "Kathleen" + ' ' + '5'
+# Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+puts "#{45628 * 7839} is the result of multiplying 45628 by 7839"
+
+# Whats the value expression
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+# True
---
+++ exercise3.rb
@@ -0,0 +1,7 @@
+puts "What is your name"
+name = 'Kathlen'
+puts "Hi #{name}"
+
+year_born = '1975'
+puts " How old are you?"
+puts "#{year_born}"
