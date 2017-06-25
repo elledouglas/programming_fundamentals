6472486a582a9de0e1a753f32fc992d33a20e4b2
Kathleen <kathleenmcclay@gmail.com>
2017-06-25 16:20:01 -0400

    Exercise 5 first commit

---
+++ exercise5.rb
@@ -0,0 +1,8 @@
+
+
+
+puts " How many pizzas would you like to order?"
+quantity = gets
+puts " How many toppings would you like pizza?"
+toppings = gets
+puts "You have ordered #{quantity} pizzas with #{toppings} toppings "
