2746b4228a0ba434167c4bbf182ad56f8e6f5bd8
Kathleen <kathleenmcclay@gmail.com>
2017-06-26 15:12:27 -0400

    Completed

--- exercise5.rb
+++ exercise5.rb
@@ -1,13 +1,43 @@
-
 quantity = 1
 
-while quantity < 1
-   quantity += 1
-puts " How many pizzas would you like to order?"
-quantity = gets
-puts " How many toppings would you like pizza?"
-toppings = gets
+puts "How many pizzas would you like to order?"
+quantity = gets.to_i
 
-puts "You have ordered #{quantity} pizzas with #{toppings} toppings "
+quantity.times do |quantity|
+quantity += 1
 
+puts "How many toppings for pizza #{quantity}"
+toppings = gets.to_i
+
+
+  puts "You have ordered #{quantity} pizza with #{toppings} toppings."
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# Mina's Lesson #
+# else means otherwise . If first condition is not true we do the else statement.
+# number_of cars = 10
+# number_of_spaces = 50
+#
+#
+#
+# perentage_utilization = (number_of_cars / number_of_spaces)
+#  * 100
+# if perentage_utilization > 95
+#   puts "The lot is full"
+# elsif perentage_utilization >= 50
+#   puts "Surge pricing in effect"
+# else puts "The lot is currently #{perentage_utilization}% full"
+# end
+#
+# surge_pricing = true if perentage_utilization >= 50
