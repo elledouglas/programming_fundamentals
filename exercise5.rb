aecd018b62e8f101b73f0c77ef95c61db4845474
Kathleen <kathleenmcclay@gmail.com>
2017-06-25 21:14:55 -0400

    Applied a while loop

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,13 @@
 
+quantity = 1
 
-
+while quantity < 1
+   quantity += 1
 puts " How many pizzas would you like to order?"
 quantity = gets
 puts " How many toppings would you like pizza?"
 toppings = gets
+
 puts "You have ordered #{quantity} pizzas with #{toppings} toppings "
+
+end
